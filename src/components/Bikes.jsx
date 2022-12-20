3a071d8fdb63d76ac41c6de059d1667bafd49eea
palmally <palmally@yandex.ru>
2022-12-21 00:07:53 +0300

    feat: fetching bikes list

--- src/components/Bikes.jsx
+++ src/components/Bikes.jsx
@@ -1,12 +1,27 @@
+import React from 'react';
 import BikeCard from './BikeCard';
-import bikes from '../assets/bikes.json';
 
 function Bikes() {
+  const [bikesList, setBikesList] = React.useState([]);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch('https://63a2225aa543280f7769c71a.mockapi.io/bikes');
+        const json = await res.json();
+        setBikesList(json);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <section>
       <h2 className="bikes__title">шоссе</h2>
       <ul className="bikes__grid">
-        {bikes.map((bike) => (
+        {bikesList.map((bike) => (
           <BikeCard key={bike.id} {...bike} />
         ))}
       </ul>

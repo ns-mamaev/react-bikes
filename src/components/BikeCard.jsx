c8e028a7975e36bba126a3c8780791afc85e50a1
palmally <palmally@yandex.ru>
2022-12-20 20:37:06 +0300

    feat:bike card

--- src/components/BikeCard.jsx
+++ src/components/BikeCard.jsx
@@ -1,20 +1,39 @@
-function BikeCard({ modelName, images: [image], colors, sizes, price }) {
+import React from 'react';
+
+function BikeCard({ modelName, images, colors, sizes, price }) {
+  const [selectedColor, setSelectedColor] = React.useState(0);
+  const [selectedSize, setSelectedSize] = React.useState(0);
+
   return (
     <li className="bike-card">
       <h4 className="bike-card__name">{modelName}</h4>
-      <img src={image} alt={modelName} className="bike-card__image" />
+      <img src={images[selectedColor]} alt={modelName} className="bike-card__image" />
       <ul className="bike-card__colors">
-        {colors.map((color) => (
-          <li className="bike-card__color-circle" style={{ backgroundColor: color }}></li>
+        {colors.map((color, i) => (
+          <li
+            className={`bike-card__color-circle ${
+              selectedColor === i ? 'bike-card__color-circle_active' : ''
+            }`}
+            style={{ backgroundColor: color }}
+            onClick={() => setSelectedColor(i)}
+          />
         ))}
       </ul>
       <ul className="bike-card__sizes">
-        {sizes.map((size) => (
-          <li className="bike-card__size-tab">{size}</li>
+        {sizes.map((size, i) => (
+          <li
+            className={`bike-card__size-tab ${
+              selectedSize === i ? 'bike-card__size-tab_active' : ''
+            }`}
+            onClick={() => setSelectedSize(i)}>
+            {size}
+          </li>
         ))}
       </ul>
-      <p className="bike-card__price">от {price} $</p>
-      <button className="bike-card__button">Добавить</button>
+      <div className="bike-card__bottom">
+        <p className="bike-card__price">от {price} $</p>
+        <button className="bike-card__cart-button">Добавить</button>
+      </div>
     </li>
   );
 }

061a5d4623bd866cab607d729712ab43d1b0713a
Nikita Mamaev <palmally@yandex.ru>
2022-12-22 17:30:31 +0300

    refactor: bikes component shows 'find nothing' message

--- src/components/Bikes.jsx
+++ src/components/Bikes.jsx
@@ -3,12 +3,21 @@ import BikeCard from './BikeCard';
 import BikeSkeleton from './BikeSkeleton';
 
 function Bikes({ list, inLoading }) {
+  const skeletons = inLoading
+    ? [...new Array(3)].map((_, index) => <BikeSkeleton key={index} />)
+    : null;
+  const bikes =
+    list.length !== 0 ? (
+      list.map((bike) => <BikeCard key={bike.id} {...bike} />)
+    ) : (
+      <p>Ничего не найдено</p>
+    );
+
   return (
     <section>
       <ul className="bikes__grid">
-        {inLoading
-          ? [...new Array(3)].map((_, index) => <BikeSkeleton key={index} />)
-          : list.map((bike) => <BikeCard key={bike.id} {...bike} />)}
+        {skeletons}
+        {bikes}
       </ul>
     </section>
   );

9a3ed9b802e5d23ae14d503fdc19dab514c4cf9b
palmally <palmally@yandex.ru>
2022-12-25 16:53:31 +0300

    feat: search Component with Redux

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -4,27 +4,43 @@ import { setSearchValue } from '../../redux/slices/filterSlice';
 import { debounce } from '../../utills/utills';
 import styles from './Search.module.scss';
 
+console.log(styles);
+
 function Search() {
   const [displayedText, setDisplayedText] = React.useState('');
   const dispatch = useDispatch();
-
-  const onSearch = React.useCallback(
-    debounce((value) => dispatch(setSearchValue(value)), 500),
-    [],
-  );
+  const inputRef = React.useRef();
 
   const onChange = (value) => {
     setDisplayedText(value);
     onSearch(value);
   };
 
+  const onClear = () => {
+    setDisplayedText('');
+    dispatch(setSearchValue(''));
+    inputRef.current.focus();
+  };
+
+  const onSearch = React.useCallback(
+    debounce((value) => dispatch(setSearchValue(value)), 500),
+    [],
+  );
+
   return (
-    <input
-      type="text"
-      placeholder="Поиск по названию..."
-      value={displayedText}
-      onChange={(e) => onChange(e.target.value)}
-    />
+    <div className={styles.root}>
+      <div className={`${styles.icon} ${styles.iconSearch}`}></div>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Поиск по названию..."
+        value={displayedText}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      {displayedText && (
+        <button onClick={onClear} className={`${styles.icon} ${styles.iconClear}`} />
+      )}
+    </div>
   );
 }
 

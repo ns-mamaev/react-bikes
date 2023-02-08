8732ac5dec9b496e0ccc9fbf3e2c3bbdcdac1ab1
palmally <palmally@yandex.ru>
2023-02-09 02:02:38 +0300

    feat: close popup with esc & click around

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { sortTypes } from '../utills/constants';
 import { setSortType } from '../redux/slices/filterSlice';
@@ -8,22 +8,46 @@ function Sort() {
   const selected = useSelector((state) => state.filter.sortTypeId);
   const selectedName = sortTypes[selected].title;
   const dispatch = useDispatch();
+  const popupRef = useRef();
 
   const onSelectType = (i) => {
     dispatch(setSortType(i));
     setPopupOpened(false);
   };
 
+  useEffect(() => {
+    const onEscClose = (e) => {
+      if (e.key === 'Escape') {
+        setPopupOpened(false);
+      }
+    };
+
+    const onClickAround = (e) => {
+      if (!e.path.includes(popupRef.current)) {
+        setPopupOpened(!popupOpened);
+      }
+    };
+
+    if (popupOpened) {
+      window.addEventListener('keydown', onEscClose);
+      document.body.addEventListener('click', onClickAround);
+    }
+    return () => {
+      window.removeEventListener('keydown', onEscClose);
+      document.body.removeEventListener('click', onClickAround);
+    };
+  }, [popupOpened]);
+
   return (
-    <div className="sort">
-      <div className="sort__label">
-        <span className="sort__label-static">Сортировать по:</span>
-        <span className="sort__selected-type" onClick={() => setPopupOpened(!popupOpened)}>
+    <div className='sort' ref={popupRef}>
+      <div className='sort__label'>
+        <span className='sort__label-static'>Сортировать по:</span>
+        <span className='sort__selected-type' onClick={() => setPopupOpened(!popupOpened)}>
           {selectedName}
         </span>
       </div>
       {popupOpened && (
-        <ul className="sort__popup">
+        <ul className='sort__popup'>
           {sortTypes.map(({ title }, i) => (
             <li
               key={title}

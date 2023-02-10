fbade2de0448e374b8b8fdab746eaea202baa2a5
Nikita Mamaev <palmally@yandex.ru>
2023-02-10 14:31:00 +0300

    feat: Bike component knows when added to cart

--- src/components/BikeCard.jsx
+++ src/components/BikeCard.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { addItem } from '../redux/slices/cartSlice';
 
-function BikeCard({ id, modelName, images, colors, sizes, price }) {
+function BikeCard({ id, modelName, images, colors, sizes, price, addedToCart }) {
   const [selectedColor, setSelectedColor] = React.useState(0);
   const [selectedSize, setSelectedSize] = React.useState(0);
   const color = colors[selectedColor];
   const image = images[selectedColor];
   const size = sizes[selectedSize];
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const isAddedToCart = useMemo(() => {
+    if (!addedToCart.length) {
+      return false;
+    }
+    const match = addedToCart.findIndex(({ options }) => {
+      console.log(options);
+      return options.color === color && options.size === size;
+    });
+    return match > -1;
+  }, [color, size, addedToCart]);
 
   const onAddToCard = () => {
     const item = { id, title: modelName, options: { color, size }, image, price };
@@ -16,10 +29,10 @@ function BikeCard({ id, modelName, images, colors, sizes, price }) {
   };
 
   return (
-    <li className='bike-card'>
-      <h4 className='bike-card__name'>{modelName}</h4>
-      <img src={image} alt={modelName} className='bike-card__image' />
-      <ul className='bike-card__colors'>
+    <li className="bike-card">
+      <h4 className="bike-card__name">{modelName}</h4>
+      <img src={image} alt={modelName} className="bike-card__image" />
+      <ul className="bike-card__colors">
         {colors.map((color, i) => (
           <li
             className={`bike-card__color-circle ${
@@ -31,7 +44,7 @@ function BikeCard({ id, modelName, images, colors, sizes, price }) {
           />
         ))}
       </ul>
-      <ul className='bike-card__sizes'>
+      <ul className="bike-card__sizes">
         {sizes.map((size, i) => (
           <li
             className={`bike-card__size-tab ${
@@ -43,10 +56,15 @@ function BikeCard({ id, modelName, images, colors, sizes, price }) {
           </li>
         ))}
       </ul>
-      <div className='bike-card__bottom'>
-        <p className='bike-card__price'>от {price} $</p>
-        <button type='button' className='bike-card__cart-button' onClick={onAddToCard}>
-          Добавить
+      <div className="bike-card__bottom">
+        <p className="bike-card__price">от {price} $</p>
+        <button
+          type="button"
+          className={`bike-card__cart-button ${
+            isAddedToCart ? '' : 'bike-card__cart-button_action_add'
+          }`}
+          onClick={isAddedToCart ? () => navigate('/cart') : onAddToCard}>
+          {isAddedToCart ? 'В корзину' : 'Добавить'}
         </button>
       </div>
     </li>
